1854921f96a4c346d4935948b3b5ad8c79e61577
Semmmetje <vanderlandsem8@gmail.com>
2026-08-22 23:09:12 +0200

    Store one crew home

--- src/main/java/me/semmmetje/banditteams/storage/Team.java
+++ src/main/java/me/semmmetje/banditteams/storage/Team.java
@@ -1,4 +1,5 @@
 package me.semmmetje.banditteams.storage;
+import org.bukkit.Location;
 import java.util.Set;
 import java.util.UUID;
-public record Team(String name, UUID leader, Set<UUID> members, boolean joinRequestsEnabled, boolean teamChatEnabled) {}
+public record Team(String name, UUID leader, Set<UUID> members, boolean joinRequestsEnabled, boolean teamChatEnabled, Location home) {}
